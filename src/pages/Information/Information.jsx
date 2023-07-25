911e6822215449468ef6ab1c9143de007c79900c
Alex <ol.shevchuk92@gmail.com>
2023-07-25 18:00:59 +0300

    deploy to vercel

--- src/pages/Information/Information.jsx
+++ src/pages/Information/Information.jsx
@@ -1,22 +1,24 @@
 const Information = () => {
   return (
-    <div>
-      <p>
-        Welcome to Ethereum Ethereum is the community-run technology powering
-        the cryptocurrency ether (ETH) and thousands of decentralized
-        applications.
-      </p>
-      <p>
-        Summary Ethereum is a network of computers all over the world that
-        follow a set of rules called the Ethereum protocol. The Ethereum network
-        acts as the foundation for communities, applications, organizations and
-        digital assets that anyone can build and use. You can create an Ethereum
-        account from anywhere, at any time, and explore a world of apps or build
-        your own. The core innovation is that you can do all this without
-        trusting a central authority that could change the rules or restrict
-        your access. Keep reading to learn more…
-      </p>
-    </div>
+    <>
+      <h1>Welcome to Ethereum</h1>
+      <div>
+        <p>
+          Ethereum is the community-run technology powering the cryptocurrency
+          ether (ETH) and thousands of decentralized applications.
+        </p>
+        <p>
+          Summary Ethereum is a network of computers all over the world that
+          follow a set of rules called the Ethereum protocol. The Ethereum
+          network acts as the foundation for communities, applications,
+          organizations and digital assets that anyone can build and use. You
+          can create an Ethereum account from anywhere, at any time, and explore
+          a world of apps or build your own. The core innovation is that you can
+          do all this without trusting a central authority that could change the
+          rules or restrict your access. Keep reading to learn more…
+        </p>
+      </div>
+    </>
   );
 };
 
